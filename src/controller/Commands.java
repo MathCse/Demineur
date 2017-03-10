2019e2b49fac25726635972b2aea716bc859e354
Vous@exemple.com <Vous@exemple.com>
2017-03-10 10:08:38 +0100

    classe Commands

---
+++ src/controller/Commands.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author mathieu
+ */
+public class Commands {
+    private char action;
+    private int i, j;
+    private char argument;
+
+    public Commands() {
+        
+    }
+    
+    public Commands(char action, int i, int j, char argument) {
+        
+    }
+}
