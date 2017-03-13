738d3677b8bc7b2e0c39b0e87aec8bb8e9b9145b
Matcoc <mat7nov96@hotmail.fr>
2017-03-13 15:15:12 +0100

    Tour de jeu

--- src/view/Demineur.java
+++ src/view/Demineur.java
@@ -18,7 +18,7 @@ public class Demineur {
         
         do{
             newGame.round();
-        } while (!newGame.isLost() || !newGame.isSuccessful() || newGame.isQuit());
+        } while (!newGame.isLost() && !newGame.isSuccessful() || newGame.isQuit());
     }
     
 }
